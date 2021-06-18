4d7f44a7e4aa3c062e25afde3504e07a547e5240
satya.dudi <satyavinay.dudi@gmail.com>
2021-06-18 19:09:04 +0530

    First Commit

---
+++ src/customer/Dao/CustomerDAO.java
@@ -0,0 +1,19 @@
+package customer.Dao;
+
+import java.util.List;
+
+
+import springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+    public List<Customer> getCustomers();
+
+    public void saveCustomer(Customer theCustomer);
+
+    public Customer getCustomer(int theId);
+
+    public void deleteCustomer(int theId);
+
+    public List<Customer> searchCustomers(String theSearchName);
+}
